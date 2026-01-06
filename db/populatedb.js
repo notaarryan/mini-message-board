72bf0397b03828bc000a70e896268e8e32b11c18
Aryan Parmar <156536547+notaarryan@users.noreply.github.com>
2026-01-06 15:31:55 -0500

    feat: completed implementation of postgres db

---
+++ db/populatedb.js
@@ -0,0 +1,24 @@
+#! /usr/bin/env node
+require("dotenv").config({
+  path: require("path").resolve(__dirname, "../.env"),
+});
+const { Client } = require("pg");
+
+const SQL = `CREATE TABLE IF NOT EXISTS messages (id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY, username VARCHAR (225), message VARCHAR(10000), added TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP);
+
+INSERT INTO messages (username, message)
+VALUES
+  ('Aryan', 'Hi There'), ('Aryan', 'Hope you like my website');`;
+
+async function main() {
+  console.log("seeding...");
+  const client = new Client({
+    connectionString: `postgresql://${process.env.POSTGRES_USERNAME}:${process.env.POSTGRES_PASSWORD}@${process.env.POSTGRES_HOST}:${process.env.POSTGRES_PORT}/${process.env.POSTGRES_DB}`,
+  });
+  await client.connect();
+  await client.query(SQL);
+  await client.end();
+  console.log("done");
+}
+
+main();
